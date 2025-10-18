4b172fa123c555a4e859110e26f48b966a9bc23d
Jeff Gutierrez <jeffgutierrez0524@gmail.com>
2025-10-18 16:43:03 -0400

    Ch. 5.6 Prof. G's SwiftUI course

--- ToDoList/ToDoListApp.swift
+++ ToDoList/ToDoListApp.swift
@@ -6,12 +6,17 @@
 //
 
 import SwiftUI
+import SwiftData
 
 @main
 struct ToDoListApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ToDoListView()
+                .modelContainer(for: ToDo.self)
         }
+    }
+    init(){
+        print(URL.applicationSupportDirectory.path(percentEncoded: false))
     }
 }
